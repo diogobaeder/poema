350e5913c8d9560e375ec07a4efedfaf3f4bbc45
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2017-04-05 02:20:20 -0300

    Adicionando arquivos preliminares do tema visual para Bootstrap

--- poema/settings.py
+++ poema/settings.py
@@ -189,6 +189,13 @@ STATIC_URL = "/static/"
 # Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/"))
 
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    # other finders..
+    'compressor.finders.CompressorFinder',
+)
+
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
@@ -239,6 +246,7 @@ if DJANGO_VERSION < (1, 9):
 
 INSTALLED_APPS = (
     'poema.theme',
+    'compressor',
 
     "django.contrib.admin",
     "django.contrib.auth",
--- poema/theme/static/poema/js/poema.js
+++ poema/theme/static/poema/js/poema.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
     var nav = $('.navbar');
     $(window).scroll(function() {
-    console.log('scroll');
         if ($(document).scrollTop() > 50) {
             nav.addClass('shrink');
         } else {
