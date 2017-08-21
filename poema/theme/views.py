0f11031086c07ebf6ada3540f33bbc0b7160088f
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2017-08-21 04:04:30 -0300

    Criando seção dos pilares

--- poema/theme/views.py
+++ poema/theme/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 
 from mezzanine.blog.models import BlogPost
+from mezzanine.pages.models import Page
 
 
 def home(request):
     posts = (BlogPost.objects.prefetch_related('categories').all()
              .order_by('-publish_date')[:9])
+    main_pillar = Page.objects.get(title='Pilares')
+    pillars = Page.objects.filter(parent=main_pillar)
 
     return render(request, 'index.html', {
         'posts': posts,
+        'pillars': pillars,
     })
